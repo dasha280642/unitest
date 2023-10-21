7eb4b7e6a72806d3bcd7ee2b686a658699e0f7e9
dasha280642 <dasha19982009@rambler.ru>
2023-10-22 01:01:57 +0300

    Test

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -1,11 +1,28 @@
 import unittest
 from utils.arrs import get
+from utils.arrs import my_slice
+
 import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(1 + 1, 2)
 
+    def test_get(self):
+        self.assertEqual(get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(get([1, 2, 3], 10, "test"), "test")
+        self.assertEqual(get([], 0, "test"), "test")
+
+    def test_slice(self):
+        self.assertEqual(my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(my_slice([1, 2, 3], 1), [2, 3])
+
+    def test_slice_empty_array(self):
+        self.assertEqual(my_slice([], 1, 3), [])
+
+    def test_slice_start(self):
+        self.assertEqual(my_slice([1, 2, 3], -10, 10), [1, 2, 3])
+        self.assertEqual(my_slice([1, 2, 3], -2, 10), [2, 3])
 if __name__ == '__main__':
     unittest.main()
 
@@ -32,17 +49,8 @@ def test_get_empty_array():
     default = "empty"
     assert get(array, index, default) == default
 
-def test_get_default_value():
-    array = [1, 2, 3, 4, 5]
-    index = 10
-    default = "not found"
-    assert get(array, index, default) == default
 
-def test_get_default_value_none():
-    array = [1, 2, 3, 4, 5]
-    index = 10
-    default = None
-    assert get(array, index, default) == default
+
 
 
 def test_get(array, index, default, expected):
